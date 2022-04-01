9bb6e90e183ca66a8dd67a19d8bb8a65b728fa41
Theodoros Grammenos <teogramm@outlook.com>
2022-04-01 18:23:34 +0300

    Add add movie function, use custom exceptions

--- libmoviesort/src/MovieDatabase.cpp
+++ libmoviesort/src/MovieDatabase.cpp
@@ -1,9 +1,10 @@
-#include "MovieDatabase.h"
+#include "MovieSort/MovieDatabase.h"
+#include "MovieSort/Exceptions.h"
 
 /**
  * Creates the movies table.
  * It contains the name of each movie and its elo.
- * @param db pointer to sqlite3 database
+ * @param db SQLiteCpp database object
  */
 void create_movies_table(SQLite::Database &db){
     auto sql_query = std::string(
@@ -17,6 +18,10 @@ void create_movies_table(SQLite::Database &db){
     stmt.executeStep();
 }
 
+/**
+ * Create table containing matches between movies.
+ * @param db SQLiteCpp database object
+ */
 void create_matches_table(SQLite::Database &db){
     auto sql_query = std::string(
             "CREATE TABLE IF NOT EXISTS matches ("
@@ -41,12 +46,34 @@ void initialize_database(SQLite::Database &db){
     create_matches_table(db);
 }
 
+using namespace MovieSort;
 /**
  * Opens a movie database in the given database file
  * @param databaseFile path to the database file
  */
 MovieDatabase::MovieDatabase(const std::string& databaseFile): db(SQLite::Database(databaseFile, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE))
 {
-    initialize_database(db);
+    try {
+        initialize_database(db);
+    }catch (SQLite::Exception &e){
+        // Rethrow exception as database error
+        throw DatabaseError(e.what());
+    }
+}
+
+void MovieDatabase::addMovie(const std::string &movieName) {
+    auto query = std::string("INSERT INTO movies(name) VALUES (?);");
+    auto stmt = SQLite::Statement(db, query);
+    stmt.bind(1, movieName);
+    try {
+        stmt.executeStep();
+    } catch (SQLite::Exception &e) {
+        auto what_string = std::string(e.what());
+        if (what_string.find("UNIQUE") != std::string::npos){
+            throw MovieAlreadyExists(movieName);
+        }else{
+            throw DatabaseError(what_string);
+        }
+    }
 }
 
