a946f7dfda65ace77bc9b311fa3d7a9098787c59
Theodoros Grammenos <teogramm@outlook.com>
2022-04-06 18:26:24 +0300

    Complete AddMovie panel

--- gui/include/AddMoviePane.h
+++ gui/include/AddMoviePane.h
@@ -8,14 +8,26 @@ namespace Ui{
 };
 
 namespace MSGui{
+
     class AddMovie: public QWidget{
         Q_OBJECT
     public:
         explicit AddMovie(QWidget* parent = nullptr);
         ~AddMovie() override;
+        enum class Result{
+            Success,
+            MovieAlreadyExists,
+            Error
+        };
     signals:
+        void wantToAddMovie(const QString& movieName);
         void closeButtonPressed();
+    private slots:
+        void addMoviesFromFile();
+    public slots:
+        void movieAddedResult(const QString& movieName, Result result);
     private:
+        bool fileLoadingInProgress = false;
         Ui::AddMovie* ui;
     };
 }
