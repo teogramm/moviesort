d411ddb508514d0f006758f03c11062c46829d42
Theodoros Grammenos <teogramm@outlook.com>
2022-04-07 20:27:54 +0300

    Add IMatch interface

--- libmoviesort/include/public/MovieSort/IMatch.h
+++ libmoviesort/include/public/MovieSort/IMatch.h
@@ -2,11 +2,16 @@
 #define MOVIESORT_IMATCH_H
 #include "Movie.h"
 
+
+namespace MovieSort {
 /**
  * An Interface used to request matches and submit results
  */
-class IMatch{
-    virtual std::pair<MovieSort::Movie, MovieSort::Movie> getMatch() = 0;
-    virtual void submitMatchResult(const std::string& movie1Name, const std::string& movie2Name, int matchResult) = 0;
-};
+    class IMatch {
+    public:
+        virtual std::pair<MovieSort::Movie, MovieSort::Movie> generateMatch() = 0;
+        virtual void
+        writeMatchResult(const std::string &movie1Name, const std::string &movie2Name, int matchResult) = 0;
+    };
+}
 #endif //MOVIESORT_IMATCH_H
