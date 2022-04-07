ed907e242bf6f7266d3e697aad5eee1c4ce2078b
Theodoros Grammenos <teogramm@outlook.com>
2022-04-07 20:27:54 +0300

    Add IMatch interface

--- libmoviesort/include/public/MovieSort/Backend.h
+++ libmoviesort/include/public/MovieSort/Backend.h
@@ -4,19 +4,19 @@
 #include <vector>
 #include <memory>
 #include "MovieSort/Movie.h"
-
+#include "MovieSort/IMatch.h"
 
 namespace MovieSort {
-    class Backend {
+    class Backend: public IMatch{
     public:
         explicit Backend(const std::string &fileName);
         // Define destructor here and implement it in the cpp file
         ~Backend();
         void addMovie(const std::string &movieName);
-        void writeMatchResult(const std::string& movie1Name, const std::string& movie2Name, int matchResult);
+        void writeMatchResult(const std::string& movie1Name, const std::string& movie2Name, int matchResult) override;
         [[nodiscard]] std::vector<Movie> getAllMovies();
         std::vector<Movie> getTopKMovies(unsigned k = 10);
-        std::pair<Movie, Movie> generateMatch();
+        std::pair<Movie, Movie> generateMatch() override;
     private:
         class impl;
         std::unique_ptr<impl> pimpl;
